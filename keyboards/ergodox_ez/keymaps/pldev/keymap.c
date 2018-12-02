148e8b8062c38636a25695b85ee62b35a441d50f
pldev <pldev@lunau.me.uk>
2018-12-02 16:22:00 +0000

    Align keymaps for easier reading.

--- keyboards/ergodox_ez/keymaps/pldev/keymap.c
+++ keyboards/ergodox_ez/keymaps/pldev/keymap.c
@@ -42,23 +42,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
 [BASE] = LAYOUT_ergodox(  // layer 0 : default
         // left hand
-        KC_ESC,         KC_1,  KC_2,   KC_3,          KC_4,          KC_5,   KC_CAPSLOCK,
-        KC_TAB,         KC_Q,  KC_W,   KC_E,          KC_R,          KC_T,   KC_LBRACKET,
-        KC_LSHIFT,       KC_A,  KC_S,   ALT_T(KC_D),   CTL_T(KC_F),   KC_G,
-        KC_LCTRL,       KC_Z,  KC_X,   KC_C,          KC_V,          KC_B,   KC_MINUS,
-        KC_GRV,         KC_NONUS_BSLASH,      KC_QUOT,  KC_NONUS_HASH,TT(SYMB),
-                                              KC_LGUI,  KC_HOME,
-                                                                  KC_INSERT,
-                                              KC_SPACE,KC_LALT,KC_DELETE,
+        KC_ESC,    KC_1,            KC_2,    KC_3,          KC_4,        KC_5, KC_CAPSLOCK,
+        KC_TAB,    KC_Q,            KC_W,    KC_E,          KC_R,        KC_T, KC_LBRACKET,
+        KC_LSHIFT, KC_A,            KC_S,    ALT_T(KC_D),   CTL_T(KC_F), KC_G,
+        KC_LCTRL,  KC_Z,            KC_X,    KC_C,          KC_V,        KC_B, KC_MINUS,
+        KC_GRV,    KC_NONUS_BSLASH, KC_QUOT, KC_NONUS_HASH, TT(SYMB),
+                                                                      KC_LGUI, KC_HOME,
+                                                                               KC_INSERT,
+                                                            KC_SPACE, KC_LALT, KC_DELETE,
         // right hand
-        KC_PSCREEN,     KC_6,   KC_7,  KC_8,   KC_9,   KC_0,             TG(NUM),
-        KC_RBRACKET,    KC_Y,   KC_U,  KC_I,   KC_O,   KC_P,             KC_BSPACE,
-        KC_H,   CTL_T(KC_J),  ALT_T(KC_K),   KC_L,   KC_SCLN, KC_RSHIFT,
-        KC_EQUAL ,KC_N,   KC_M,  KC_COMM,KC_DOT, KC_SLSH,   KC_RCTRL,
-                          TT(SYMB), KC_LEFT,KC_DOWN,KC_UP,          KC_RIGHT,
-        KC_END,        KC_RGUI,
-             KC_PGUP,
-             KC_PGDN,KC_BSPACE, KC_ENT
+        KC_PSCREEN,  KC_6, KC_7,        KC_8,        KC_9,    KC_0,          TG(NUM),
+        KC_RBRACKET, KC_Y, KC_U,        KC_I,        KC_O,    KC_P,          KC_BSPACE,
+                     KC_H, CTL_T(KC_J), ALT_T(KC_K), KC_L,    KC_SCLN,       KC_RSHIFT,
+        KC_EQUAL ,   KC_N, KC_M,        KC_COMM,     KC_DOT,  KC_SLSH,       KC_RCTRL,
+                                        TT(SYMB),    KC_LEFT, KC_DOWN,KC_UP, KC_RIGHT,
+        KC_END,  KC_RGUI,
+        KC_PGUP,
+        KC_PGDN, KC_BSPACE, KC_ENT
     ),
 /* Keymap 1: Symbol Layer
  *
@@ -84,20 +84,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // SYMBOLS
 [SYMB] = LAYOUT_ergodox(
        // left hand
-       VRSN,    KC_F1,   KC_F2,   KC_F3,   KC_F4,           KC_F5,   KC_TRNS,
-       KC_TRNS, KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR,         KC_PIPE, KC_TRNS,
-       KC_TRNS, KC_HASH, KC_DLR,  KC_TRNS, KC_TRNS,         KC_GRV,
+       VRSN,    KC_F1,   KC_F2,          KC_F3,          KC_F4,           KC_F5,                 KC_TRNS,
+       KC_TRNS, KC_EXLM, KC_AT,          KC_LCBR,        KC_RCBR,         KC_PIPE,               KC_TRNS,
+       KC_TRNS, KC_HASH, KC_DLR,         KC_TRNS,        KC_TRNS,         KC_GRV,
        KC_TRNS, KC_PERC, LSFT(KC_MINUS), LSFT(KC_EQUAL), KC_NONUS_BSLASH, LSFT(KC_NONUS_BSLASH), KC_TRNS,
-          EPRM, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-                                       RGB_MOD,KC_TRNS,
-                                               KC_TRNS,
-                               RGB_VAD,RGB_VAI,KC_TRNS,
+          EPRM, KC_TRNS, KC_TRNS,        KC_TRNS,        KC_TRNS,
+                                                                          RGB_MOD,               KC_TRNS,
+                                                                                                 KC_TRNS,
+                                                         RGB_VAD,         RGB_VAI,               KC_TRNS,
        // right hand
-       KC_TRNS,  KC_F6,       KC_F7,       KC_F8,         KC_F9,    KC_F10,   KC_F11,
-       KC_TRNS,  KC_LBRACKET, KC_RBRACKET, KC_UP,         KC_MINUS, KC_EQUAL, KC_F12,
-                 KC_LEFT_CURLY_BRACE,     KC_LEFT,     KC_DOWN,       KC_RIGHT, KC_TRNS, KC_TRNS,
-       KC_TRNS,  KC_RIGHT_CURLY_BRACE,     KC_QUOT,     KC_NONUS_HASH, LSFT(KC_QUOT),  KC_PIPE/*uk tilde*/, KC_TRNS,
-                              KC_TRNS,     KC_TRNS,       KC_TRNS,  KC_TRNS, KC_TRNS,
+       KC_TRNS,  KC_F6,                KC_F7,       KC_F8,         KC_F9,         KC_F10,              KC_F11,
+       KC_TRNS,  KC_LBRACKET,          KC_RBRACKET, KC_UP,         KC_MINUS,      KC_EQUAL,            KC_F12,
+                 KC_LEFT_CURLY_BRACE,  KC_LEFT,     KC_DOWN,       KC_RIGHT,      KC_TRNS,             KC_TRNS,
+       KC_TRNS,  KC_RIGHT_CURLY_BRACE, KC_QUOT,     KC_NONUS_HASH, LSFT(KC_QUOT), KC_PIPE/*uk tilde*/, KC_TRNS,
+                                       KC_TRNS,     KC_TRNS,       KC_TRNS,       KC_TRNS,             KC_TRNS,
        KC_TRNS, KC_TRNS,
        KC_TRNS,
        KC_TRNS,  KC_TRNS,  KC_TRNS
@@ -146,7 +146,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [NUM] = LAYOUT_ergodox(
        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
        KC_TRNS, RGB_TOG, RGB_SLD, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-       KC_TRNS, RGB_HUD, RGB_HUI, KC_TRNS, KC_TRNS, KC_TRNS, 
+       KC_TRNS, RGB_HUD, RGB_HUI, KC_TRNS, KC_TRNS, KC_TRNS,
        KC_TRNS, RGB_VAD, RGB_VAI, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
        KC_TRNS, RGB_MOD, KC_TRNS, KC_TRNS, KC_TRNS,
                                                     KC_TRNS, KC_TRNS,
