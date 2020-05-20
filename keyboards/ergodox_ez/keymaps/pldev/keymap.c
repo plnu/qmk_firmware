0a6284716fc6f953414c6f8b93ef8628fd173873
pldev <pldev@lunau.me.uk>
2020-05-20 21:38:21 +0100

    shift-insert key and an extra home/end binding

--- keyboards/ergodox_ez/keymaps/pldev/keymap.c
+++ keyboards/ergodox_ez/keymaps/pldev/keymap.c
@@ -43,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = LAYOUT_ergodox(  // layer 0 : default
         // left hand
         KC_ESC,    KC_1,            KC_2,    KC_3,          KC_4,        KC_5, KC_CAPSLOCK,
-        KC_TAB,    KC_Q,            KC_W,    KC_E,          KC_R,        KC_T, KC_LBRACKET,
+        KC_TAB,    KC_Q,            KC_W,    KC_E,          KC_R,        KC_T, LSFT(KC_INSERT),
         KC_LSHIFT, KC_A,            KC_S,    ALT_T(KC_D),   CTL_T(KC_F), KC_G,
         KC_LCTRL,  KC_Z,            KC_X,    KC_C,          KC_V,        KC_B, KC_MINUS,
         KC_GRV,    KC_NONUS_BSLASH, KC_QUOT, KC_NONUS_HASH, TT(SYMB),
@@ -52,9 +52,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                             KC_SPACE, KC_LALT, KC_DELETE,
         // right hand
         KC_PSCREEN,  KC_6, KC_7,        KC_8,        KC_9,    KC_0,          TG(NUM),
-        KC_RBRACKET, KC_Y, KC_U,        KC_I,        KC_O,    KC_P,          KC_BSPACE,
+        KC_HOME,     KC_Y, KC_U,        KC_I,        KC_O,    KC_P,          KC_BSPACE,
                      KC_H, CTL_T(KC_J), ALT_T(KC_K), KC_L,    KC_SCLN,       KC_RSHIFT,
-        KC_EQUAL ,   KC_N, KC_M,        KC_COMM,     KC_DOT,  KC_SLSH,       KC_RCTRL,
+        KC_END ,     KC_N, KC_M,        KC_COMM,     KC_DOT,  KC_SLSH,       KC_RCTRL,
                                         TT(SYMB),    KC_LEFT, KC_DOWN,KC_UP, KC_RIGHT,
         KC_END,  KC_RGUI,
         KC_PGUP,
